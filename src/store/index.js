8ba20fd2822531b3962e4cf4291ea0a595b2ed08
Asher Azriel Ginting <asherginting3@gmail.com>
2022-03-01 21:42:09 +0700

    feat: add case decrement

--- src/store/index.js
+++ src/store/index.js
@@ -9,7 +9,10 @@ const reducer = (state = initialState, action) => {
     console.log('reducer berjalan', action);
     switch(action.type){
         case 'INCREMENT':
-        return Object.assign({}, state, {count : state.count + 1})    
+        return Object.assign({}, state, {count : state.count + 1})
+        
+        case 'DECREMENT':
+        return Object.assign({}, state, {count : state.count - 1})    
     }
     return state
 }
