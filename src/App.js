c7ea5630becc024a770bb6f6f158faefd2620c67
Asher Azriel Ginting <asherginting3@gmail.com>
2022-03-01 21:21:15 +0700

    chore: add store

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter';
+import store from './store'
 
 class App extends Component {
   state = {  } 
   render() { 
     return (
       <div className='App'>
-        <Counter count='0'/>
+        <Counter store={store}/>
       </div>
     );
   }
